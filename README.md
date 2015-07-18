49297bbd4cbc312ddfde7d3ebc58457a4fe69c92
nonnycode <miknonny@gmail.com>
2015-07-18 23:04:21 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-## IP Monster.
-Extracts Ips from large files that can not be buffered in memory.
+## Pattern Expresso.
+Extracts regular expression patterns from large files that can not be buffered in memory. 
+
+Install
+$ npm install -g pattern-expresso
+
+Usage
+pattern-expresso <source> <destination> <option>
+
+Options.
+ipv4: extract ipv4 addresses form file.
+ipv6: extracts ipv6 address from  file.
+url: extracts url from file.
+email: extract email from file.
+
+Example
+$ pattern-expresso large_file.txt dest_file.txt ipv4
+
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+'use strict';
+
 var fs = require('fs');
 var lineReader = require('line-reader');
 var Spinner = require('cli-spinner').Spinner;
@@ -8,8 +10,8 @@ var spinner = new Spinner('processing... %s');
 spinner.setSpinnerString('|/-\\');
 spinner.start();
 
-_ipJuicer = {
-  get: function (inFile, outFile, pattern) {
+var _finder = {
+  extract: function (inFile, outFile, pattern) {
     lineReader.eachLine(inFile, function (line) {
       if (line.match(pattern) !== null) {
         fs.appendFile(outFile, line.match(pattern)[0] + '\n', function (err) {
@@ -23,38 +25,31 @@ _ipJuicer = {
   }
 }
 
-var mainJuicer = function () {
+var main = function () {
   var src = process.argv[2];
   var dest = process.argv[3];
   var patternOption = process.argv[4];
   if (process.argv.length < 3) {
-    console.log('invalid no of arguments entered');
+    console.log('Usage: $ pattern-expresso <source> <destination> <option>');
     process.exit(1);  
   }
   if (regexPatterns[patternOption]) {
     if (patternOption === 'ipv4') {
-      _ipJuicer.get(src, dest, regexPatterns.ipv4);
+      _finder.extract(src, dest, regexPatterns.ipv4);
     }
     if (patternOption === 'ipv6') {
-      _ipJuicer.get(src, dest, regexPatterns.ipv6);
+      _finder.extract(src, dest, regexPatterns.ipv6);
     }
     if (patternOption === 'email') {
-      _ipJuicer.get(src, dest, regexPatterns.email);
+      _finder.extract(src, dest, regexPatterns.email);
     }
     if (patternOption === 'url') {
-      _ipJuicer.get(src, dest, regexPatterns.url)
+      _finder.extract(src, dest, regexPatterns.url)
     }
   } else {
-    console.log('regex pattern is not valid.');
-    process.exit(1)
+      console.log('Usage: $ pattern-expresso <source> <destination> <option>');
+      process.exit(1)
   }
 }
 
-mainJuicer();
-
-/**
- * Draw header
- * @param  {string} left  This is the text to go on the left
- * @param  {string} right This is the text for the right
- * @return {void}
- */
+main();
